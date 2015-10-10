e7ba86202a95f80754b068beb4ba5bd5f6bee788
Jack <infinity.jacksparrow@gmail.com>
2015-10-11 02:37:25 +0300

    add 5th question

---
+++ models/qdata/qdata_1_5.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\models\qdata;
+
+use app\models\IQData;
+
+class Qdata_1_5 implements IQData
+{
+    static public function data()
+    {
+        $rules = [
+            'hint1'    => 'Подсказка 1',
+            'hint2'    => 'Подсказка 2',
+            'question' => 'Вопрос номер 5',
+            'answer'   => [
+                'answer_type'  => 'text',
+                'right_answer' => ['ответ5'],
+            ]
+
+        ];
+        return $rules;
+    }
+}
